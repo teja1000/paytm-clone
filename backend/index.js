061b4384020e5395b29c85c224a623f33cc65f2e
teja1000 <99501668+teja1000@users.noreply.github.com>
2024-02-26 12:01:55 +0530

    signup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const cors=require("cors");
+const mainRouter=require("./routes/index");
+
+app.use(cors());
+app.use(express.json);
+
+
+const app=express();
+app.request("/api/v1",mainRouter);
+
+app.listen(3000);
 
 
